e5cb544083bedf7f6ce9a6a298ad4a463eca8762
calamarcin <katagirib@outlook.com>
2017-06-21 11:30:11 -0500

    base

---
+++ baseprueba/queryPreg.sql
@@ -0,0 +1,5 @@
+insert into preguntas (preguntas,inA,inB,inC,inD) values ("i want some_.","1","taquitos","torta","burgers","chicken");
+insert into preguntas (preguntas,inA,inB,inC,inD) values ("i want some_.","2","taquitos","torta","burgers","chicken");
+insert into preguntas (preguntas,inA,inB,inC,inD) values ("i want some_.","3","taquitos","torta","burgers","chicken");
+insert into preguntas (preguntas,inA,inB,inC,inD) values ("i want some_.","4","taquitos","torta","burgers","chicken");
+insert into preguntas (preguntas,inA,inB,inC,inD) values ("i want some_.","5","taquitos","torta","burgers","chicken");
