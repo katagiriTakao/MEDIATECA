9f44f3fd1f99e7a8b03eb97434ab6801bc44c568
Unknown <katagirib@outlook.com>
2017-06-29 14:59:45 -0500

    29

---
+++ baseprueba/querydatos.sql
@@ -0,0 +1,15 @@
+insert into asesor (id_asesor,nombre,grupos) VALUES ('312756','Katagiri Takao','405,510,603'),('213541','Valencia Isac','401,501,616');
+insert into temas (id_temas,tema) VALUES ('123edf','pronunciation'),('432frt','gérondif');
+insert into catalogo (id_materiales,nombre_mat,descripc,id_tema) VALUES ('14123a','rich english','english for snobs','123edf'),('4312l','baguette','bolillo frances','432frt');
+insert into profe_comen (comentario) VALUES ('JAJAJAJAJAJAJAJA este vato que?');
+insert into alumno_comen (comentario) VALUES ('y ame voy a aplicar');
+insert into registroresp (registro_res,registros) VALUES ('in','A,B,C,D,A,C,A,');
+insert into respu (id_respues,respues) VALUES ('in','A,B,C,D,B,C,A,');
+insert into 
+	
+
+
+
+
+
+
